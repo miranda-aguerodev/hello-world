d26262cc65c4fe2009cf54142a7c497f0d21c0d1
Miranda Agüero <maguerog070@ulacit.ed.cr>
2025-04-08 23:05:47 -0600

    Agregar programa de mayoría de edad

---
+++ mayor_menor.py
@@ -0,0 +1,13 @@
+nombre=""
+edad=0
+
+nombre=input("Escriba su nombre: ")
+edad = int(input("Escriba su edad"))
+
+print("Hola", nombre)
+
+if(edad>18):
+    print("Usted es mayor de edad: ")
+
+else:
+    print("Usted es menor de edad")
